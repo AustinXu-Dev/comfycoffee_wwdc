6e37029ad7f9829154a21750d98991efe732ce02
Skypoppi <austin.xu.me@gmail.com>
2024-03-29 16:48:11 +0700

    modify logo

--- ViewModel/CoffeeNavigationData.swift
+++ ViewModel/CoffeeNavigationData.swift
@@ -3,8 +3,8 @@ import SwiftUI
 struct CoffeeNavigationData {
     let mainData = [
         DataModel(text: "Prepare Coffees", description: "Make Coffee Step by Step", image: "cup.and.saucer",viewType: .explore),
-        DataModel(text: "Daily Checkin",description: "Calculate your consumption",image: "books.vertical", viewType: .daily),
-        DataModel(text: "Coffee Beans", description: "Learn more about coffee beans", image: "calendar.badge.checkmark", viewType: .coffeebeans),
+        DataModel(text: "Daily Checkin",description: "Calculate your consumption",image: "calendar.badge.checkmark", viewType: .daily),
+        DataModel(text: "Coffee Beans", description: "Learn more about coffee beans", image: "books.vertical", viewType: .coffeebeans),
     ]
     func subView(for id: DataModel.ID) -> ViewType?{
         guard let subView = mainData.first(where: { $0.id == id}) else{
